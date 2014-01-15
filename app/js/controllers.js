6775accaa4454c824ba0f32b9494695af5ff22e7
vivek <vivekkaushal27@yahoo.co.in>
2014-01-15 17:33:15 +0530

    Step 7

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,28 @@
 var phonecatApp = angular.module('phonecatApp', []);
+var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
-	$http.get('phones/phones.json').success(function(data) {
-		$scope.phones = data;
-	});
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', 
+	function ($scope, $http) {
+		$http.get('../phones/phones.json').success(function(data) {
+			$scope.phones = data;
+		});
 
-	$scope.orderProp='age';
-});
+		$scope.orderProp = 'age';
+	}]);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http', 
+	function ($scope, $http) {
+		$http.get('../phones/phones.json').success(function (data) {
+			$scope.phones = data;
+		});
+
+		$scope.orderProp = 'age';
+	}]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+	function($scope, $routeParams, $http) {
+		$http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+			$scope.phone = data;
+		});
+	}
+	]);
